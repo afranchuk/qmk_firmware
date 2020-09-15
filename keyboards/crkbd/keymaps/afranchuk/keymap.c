1e68fd3289ea0e19f01e776e90908fb7d8dfb40b
Alex Franchuk <afranchuk@caci.com>
2020-09-14 22:14:04 -0400

    Rearrange modifiers.

--- keyboards/crkbd/keymaps/afranchuk/keymap.c
+++ keyboards/crkbd/keymaps/afranchuk/keymap.c
@@ -1,4 +1,6 @@
 #include QMK_KEYBOARD_H
+#include <stdio.h>
+#include "crkbd.h"
 
 #ifdef RGBLIGHT_ENABLE
 // Following line allows macro to read current RGB settings
@@ -12,9 +14,9 @@ extern uint8_t is_master;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 3
+#define _SYMBOLS 1
+#define _NAV 2
+#define _META 3
 
 /*
 typedef enum {
@@ -70,7 +72,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 };
 */
 
-enum custom_keycodes { QWERTY = SAFE_RANGE, LOWER, RAISE, ADJUST };
+enum custom_keycodes { QWERTY = SAFE_RANGE, SYMBOLS, NAV, META };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -78,25 +80,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-        KC_LGUI, MO(1), KC_SPC, KC_LSHIFT, MO(2), KC_LALT
+        KC_LALT, KC_LSHIFT, KC_SPC, MO(_SYMBOLS), MO(_NAV), KC_LGUI
     ),
-    [_LOWER] = LAYOUT_split_3x6_3(
-        KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_PSCR,
-        KC_NO, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_SLCK,
-        KC_LCTL, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_DEL, KC_INS, KC_NO, KC_NO, KC_NO, KC_PAUS,
-        KC_LGUI, KC_TRNS, KC_SPC, KC_LSHIFT, MO(3), KC_LALT
-    ),
-    [_RAISE] = LAYOUT_split_3x6_3(
+    [_SYMBOLS] = LAYOUT_split_3x6_3(
         KC_ESC, KC_PLUS, KC_MINS, KC_EXLM, KC_UNDS, KC_LCBR, KC_RCBR, KC_7, KC_8, KC_9, KC_HASH, KC_BSPC,
         KC_AT, KC_ASTR, KC_SLSH, KC_CIRC, KC_EQL, KC_LPRN, KC_RPRN, KC_4, KC_5, KC_6, KC_TILD, KC_DLR,
         KC_LCTL, KC_PERC, KC_AMPR, KC_PIPE, KC_GRV, KC_LBRC, KC_RBRC, KC_1, KC_2, KC_3, KC_0, KC_BSLS,
-        KC_LGUI, MO(3), KC_SPC, KC_LSHIFT, KC_TRNS, KC_LALT
+        KC_LALT, KC_LSHIFT, KC_SPC, KC_TRNS, MO(_META), KC_LGUI
     ),
-    [_ADJUST] = LAYOUT_split_3x6_3(
+    [_NAV] = LAYOUT_split_3x6_3(
+        KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_PSCR,
+        KC_NO, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_SLCK,
+        KC_LCTL, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_DEL, KC_INS, KC_NO, KC_NO, KC_NO, KC_PAUS,
+        KC_LALT, KC_LSHIFT, KC_SPC, MO(_META), KC_TRNS, KC_LGUI
+    ),
+    [_META] = LAYOUT_split_3x6_3(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RESET, EEP_RST, KC_NO, KC_NO, KC_NO, KC_NO,
         RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO
+        KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO
     )
 };
 // clang-format on
@@ -106,15 +108,6 @@ int RGB_current_mode;
 void persistent_default_layer_set(uint16_t default_layer) {
     eeconfig_update_default_layer(default_layer);
     default_layer_set(default_layer);
-}
-
-// Setting ADJUST layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-    if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-        layer_on(layer3);
-    } else {
-        layer_off(layer3);
-    }
 }
 
 void matrix_init_user(void) {
@@ -130,28 +123,33 @@ void matrix_init_user(void) {
 // SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
 #ifdef SSD1306OLED
 
-// When add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-// void        set_keylog(uint16_t keycode, keyrecord_t *record);
-// const char *read_keylog(void);
-// const char *read_keylogs(void);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
+const char *read_layer_state(void) {
+    switch (layer_state) {
+        case 0:
+            return "Layer: Default";
+        case 1 << _SYMBOLS:
+            return "Layer: Symbols";
+        case 1 << _NAV:
+            return "Layer: Nav";
+        case 1 << _META | 1 << _NAV:
+        case 1 << _META | 1 << _SYMBOLS:
+            return "Layer: Meta";
+    }
+    return "Layer: Unknown";
+}
 
 const char *read_logo(void) {
-  static char logo[] = {
+    // clang-format off
+    static char logo[] = {
       0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 
       0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 
       0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 
       0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x80, 0x80, 0x80, 0x80, 0x80,
       0};
+    // clang-format on
 
-  return logo;
+    return logo;
 }
-
 
 void matrix_scan_user(void) { iota_gfx_task(); }
 
@@ -203,37 +201,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // set_timelog();
     }
 
-    switch (keycode) {
-        case QWERTY:
-            if (record->event.pressed) {
-                persistent_default_layer_set(1UL << _QWERTY);
-            }
-            return false;
-        case LOWER:
-            if (record->event.pressed) {
-                layer_on(_LOWER);
-                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_LOWER);
-                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-        case RAISE:
-            if (record->event.pressed) {
-                layer_on(_RAISE);
-                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_RAISE);
-                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-        case ADJUST:
-            if (record->event.pressed) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
-            }
-            return false;
-    }
     return true;
 }
--- keyboards/crkbd/keymaps/afranchuk/rules.mk
+++ keyboards/crkbd/keymaps/afranchuk/rules.mk
@@ -19,11 +19,4 @@ SLEEP_LED_ENABLE = no
 #TAP_DANCE_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./glcdfont.c \
-        ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        # ./lib/logo_reader.c \
-        # ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+SRC +=  ./glcdfont.c
